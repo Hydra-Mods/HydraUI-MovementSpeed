a56143006052786cf471c71bdb5ea330bec87209
Hydra <ryanandrews135@gmail.com>
2021-04-21 13:03:14 -0600

    1.04

--- vUI_MovementSpeed/vUI_MovementSpeed.lua
+++ vUI_MovementSpeed/vUI_MovementSpeed.lua
@@ -4,9 +4,8 @@ end
 
 local vUI, GUI, Language, Assets, Settings = vUIGlobal:get()
 
-local floor = floor
 local GetUnitSpeed = GetUnitSpeed
-local Label = STAT_SPEED
+local STAT_SPEED = STAT_SPEED
 
 local OnMouseUp = function()
 	ToggleCharacter("PaperDollFrame")
@@ -14,21 +13,20 @@ end
 
 local Update = function(self)
 	local Speed = GetUnitSpeed("player") / 7 * 100
-	self.Text:SetFormattedText("|cFF%s%s:|r |cFF%s%.0f%%|r", Settings["data-text-label-color"], Label, Settings["data-text-value-color"], Speed)
+	
+	self.Text:SetFormattedText("|cFF%s%s:|r |cFF%s%.0f%%|r", Settings["data-text-label-color"], STAT_SPEED, Settings["data-text-value-color"], Speed)
 end
 
 local OnEnable = function(self)
 	self:RegisterUnitEvent("UNIT_STATS", "player")
 	self:SetScript("OnUpdate", Update)
 	self:SetScript("OnMouseUp", OnMouseUp)
-	
 	self:Update()
 end
 
 local OnDisable = function(self)
 	self:SetScript("OnUpdate", nil)
 	self:SetScript("OnMouseUp", nil)
-	
 	self.Text:SetText("")
 end
 
